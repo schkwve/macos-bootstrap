4ee8ee58e4c9e5e853ab54f91f9f4d8926d01cc5
Jozef Nagy <schkwve@gmail.com>
2023-10-30 22:16:39 +0100

    Basic install script

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+## Colors
+red="\e[1;31m"
+green="\e[1;32m"
+blue="\e[1;34m"
+reset="\e[1;0m"
+
+## Functions
+install_cask() {
+	printf "Installing $1..."
+	brew install --cask $1 >/dev/null || exit 1
+}
+
+install_pkg() {
+	printf "Installing $1..."
+	brew install $1 >/dev/null || exit 1
+}
+
+# Check if we're actually running under macOS
+uname=$(uname -s)
+if [ $uname != "Darwin" ]; then
+	printf "${red}[!]${reset} This script is supported only on macOS.\n"
+	exit 1
+fi
+
+# Check for XCode Command Line Tools
+xcode-select -p >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	printf "XCode Command Line Tools are already installed, skipping...\n"
+else
+	printf "Installing XCode Command Line Tools...\n"
+	xcode-select --install >/dev/null || exit 1
+fi
+
+# Check if brew is installed
+if [ -x "$(command -v brew)" ]; then
+	printf "Brew is already installed, skipping...\n"
+else
+	printf "Installing brew...\n"
+	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh >/dev/null || exit 1
+fi
+
+# Update brew
+printf "Running brew update...\n"
+brew update >/dev/null
+
+# Install basic packages
+install_cask firefox
+install_cask betterdiscord-installer
+install_cask vlc
+install_cask lulu
+install_pkg neovim
+
+# Install BetterDiscord
+echo "Running BetterDiscord Installer..."
+xattr -d com.apple.quarantine /Applications/BetterDiscord.app >/dev/null 2>&1
+open /Applications/BetterDiscord.app
+
+# Copy config dotfiles
+echo "Copying configuration files..."
+touch ~/.hushlogin
+cp -r dots/. ~/
